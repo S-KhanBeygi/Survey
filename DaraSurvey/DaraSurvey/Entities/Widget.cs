cd886545a35a195456226a2d5799c9232c97d4b1
Sohail AhmadKhanBeygi <sohail_ahmadkhanbeygi@yahoo.com>
2022-04-26 07:13:34 +0430

    widget

---
+++ DaraSurvey/DaraSurvey/Entities/Widget.cs
@@ -0,0 +1,27 @@
+﻿using DaraSurvey.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaraSurvey.Entities
+{
+    public class Widget : EntityBase<int>
+    {
+        public string WidgetData { get; set; }
+    }
+
+    // ----------------------
+
+    public class WidgetConfiguration : IEntityTypeConfiguration<Widget>
+    {
+        public void Configure(EntityTypeBuilder<Widget> builder)
+        {
+            builder
+                .ToTable("Widget")
+                .HasQueryFilter(o => !o.Deleted.HasValue);
+        }
+    }
+}
